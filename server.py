43577419ccbb0429c6ba9b619ed32a269849be0c
Brian Rodriguez <Rodriguezantoniobrian@gmail.com>
2023-02-14 18:33:04 -0500

    Added Comments

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ app.secret_key = 'watagatapitusberry'
 def index():
     return render_template("index.html")
 
+# The code below processes the form data inputed by the user and stores the information in session
 @app.route('/processing', methods=['POST'])
 def processing():
     print(request.form)
@@ -14,12 +15,14 @@ def processing():
     session['location'] = request.form['location']
     session['language'] = request.form['language']
     session['comment'] = request.form['comment']
-    return redirect('/results')
+    return redirect('/results')  # This redirects our user to the results page on Submit. The user never sees this '/processing' url
 
+# After Submit on form "results.html" is rendered and displayed to the user
 @app.route('/results')
 def results():
     return render_template("results.html")
 
+# In order to not have previous session information stored, this code below removes the session, and redirects back to the index route
 @app.route('/destroy')
 def destroy():
     session.clear()
